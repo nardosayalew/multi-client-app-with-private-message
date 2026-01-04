9b3c8408cc04628f9d477c6eb717672bcc613f16
saron <saronchane@gmail.com>
2026-01-04 14:53:23 +0300

    making files

---
+++ Calculator/src/CalculatorInterface.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface CalculatorInterface extends Remote {
+    double add(double a, double b) throws RemoteException;
+    double subtract(double a, double b) throws RemoteException;
+    double multiply(double a, double b) throws RemoteException;
+    double divide(double a, double b) throws RemoteException;
+}
